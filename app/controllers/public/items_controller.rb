bccafe741628b2557e74be7e25733ffeb4724fae
EC2 Default User <ec2-user@ip-172-31-44-252.ec2.internal>
2021-05-27 10:13:18 +0000

    3

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,8 @@
 class Public::ItemsController < Public::ApplicationController
-  skip_before_action :authenticate_end_user, only: [:index]
+  skip_before_action :authenticate_end_user!, only: [:index]
 
   def index
+    @items = Item.all
   end
 
   def show
